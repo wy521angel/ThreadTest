848a37e06b8324dac73a1f98ee0187bbfeae716e
DESKTOP-NO9V8JL\dell <693962802@qq.com>
2020-01-02 23:43:21 +0800

    新增Java死锁测试

--- app/src/main/java/com/example/wy521angel/threadtest/DeadLockTest.java
+++ app/src/main/java/com/example/wy521angel/threadtest/DeadLockTest.java
@@ -1,4 +1,44 @@
 package com.example.wy521angel.threadtest;
 
 public class DeadLockTest {
+
+    public static void main(String[] args) {
+        final Object lockA = new Object();
+        final Object lockB = new Object();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lockA) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+//                    synchronized (lockB) {
+//                    }
+                    System.out.println("finish A");
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lockB) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+//                    synchronized (lockA) {
+//                    }
+                    System.out.println("finish B");
+                }
+            }
+        }).start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
